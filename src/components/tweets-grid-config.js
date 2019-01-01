2f6d917fe3015b278532654cdbb40fefde35ce82
Pedro Queiroz <pedroxsqueiroz@gmail.com>
2019-01-01 13:06:54 -0200

    Adjusting page padding and configurations scrolling

--- src/components/tweets-grid-config.js
+++ src/components/tweets-grid-config.js
@@ -39,41 +39,42 @@ class TweetsGridConfig extends Component{
 
     render(){
         return (
-            <div className="container">
-                <div className="row">
-                    <h2> Configurations</h2>
-                </div>
-
-                <div class="row"> <h3> Theme </h3> </div>
-                
-                <div class="row">
-                    <div class="form-group col-md-6">
-                        <select class="form-control" onChange={this.changeTheme}>
-                            <option value="minimalist" selected={ThemeSwitchableComponent.currentTheme == 'minimalist'}> minimalist </option>
-                            <option value="ocean" selected={ThemeSwitchableComponent.currentTheme == 'ocean'}> ocean </option>
-                            <option value="dark" selected={ThemeSwitchableComponent.currentTheme == 'dark'}> dark </option>
-                        </select>
+            <div className="container grid-config-page">
+                <div class="grid-config">
+                    <div className="row">
+                        <h2> Configurations</h2>
                     </div>
+
+                    <div class="row"> <h3> Theme </h3> </div>
                     
-                </div>
-
-                <hr class="row"/>
-                
-                <div class="row"> <h3> TimeLines </h3> </div>
-
-                <div className="config-container row">
-                    {this.state.timeLinesConfig.map((config, index) => 
-                        <div className="time-line-config-container">
-                            <TimeLineConfig
-                                configId={config.id}
-                                userName={config.userName}
-                                maxTweets={config.maxTweets}
-                                limitDate={config.limitDate}
-                            /> 
+                    <div class="row">
+                        <div class="form-group col-md-6">
+                            <select class="form-control" onChange={this.changeTheme}>
+                                <option value="minimalist" selected={ThemeSwitchableComponent.currentTheme == 'minimalist'}> minimalist </option>
+                                <option value="ocean" selected={ThemeSwitchableComponent.currentTheme == 'ocean'}> ocean </option>
+                                <option value="dark" selected={ThemeSwitchableComponent.currentTheme == 'dark'}> dark </option>
+                            </select>
                         </div>
-                    )}
-                </div>
+                        
+                    </div>
 
+                    <hr class="row"/>
+                    
+                    <div class="row"> <h3> TimeLines </h3> </div>
+
+                    <div className="config-container row">
+                        {this.state.timeLinesConfig.map((config, index) => 
+                            <div className="time-line-config-container">
+                                <TimeLineConfig
+                                    configId={config.id}
+                                    userName={config.userName}
+                                    maxTweets={config.maxTweets}
+                                    limitDate={config.limitDate}
+                                /> 
+                            </div>
+                        )}
+                    </div>
+                </div>
             </div>
         );
     }
